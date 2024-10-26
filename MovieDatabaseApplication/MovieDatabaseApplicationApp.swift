16b8eec91d4b26a148dd9360f03321fef4704e69
kartikeyagit <kartikeya85@gmail.com>
2024-10-26 16:57:47 +0530

    Initial Commit

---
+++ MovieDatabaseApplication/MovieDatabaseApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseApplicationApp.swift
+//  MovieDatabaseApplication
+//
+//  Created by Kartikeya Singh on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabaseApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
